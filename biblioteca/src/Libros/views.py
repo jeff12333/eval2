d6e8b0bc7e3557b1810282d041ed20c9dda89096
jeff12333 <jefferson.bautista@tecsu.edu.pe>
2025-09-17 17:15:43 -0500

    asda

--- biblioteca/src/Libros/views.py
+++ biblioteca/src/Libros/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Libros as Item
 
-# Create your views here.
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'Libros/item_list.html', {'items': items})
